18e92e22b500eb42baaaf12d70d60552851e5a2c
ErikHatesHw <114367948+ErikEatsDoritos@users.noreply.github.com>
2025-09-09 12:24:21 -0400

    Update Main.java

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(5);
         FizzBuzz.main(args);
-        System.out.println("Hello world")
+        System.out.println("Hello world");
     }
 
 
